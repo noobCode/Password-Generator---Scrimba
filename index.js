7021c592dc01eb206ad0db644b72a854b763a96c
ethan <ethanganas@outlook.com>
2025-09-22 19:52:18 -0500

    added copy text functionalty

--- index.js
+++ index.js
@@ -36,3 +36,18 @@ function assign() {
     passwordOptionTwo.textContent = generate()
     
 }
+
+function copy() {
+
+    let copyText = querySelector(".b2")
+
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value)
+
+    // Alert the copied text
+    alert("Copied the text: " + copyText.value);
+}
